8403a161ef198e4bd9a521947673e5cdee48e62c
ryudung <rhkdgus2002@gmail.com>
2019-01-22 17:07:58 +0900

    [review] 1~4장 복습

---
+++ src/test/java/com/zummore/effectivejava/ryudung/ch04/item20/domain/VendingService.java
@@ -0,0 +1,8 @@
+package com.zummore.effectivejava.ryudung.ch04.item20.domain;
+
+public interface VendingService {
+    void start();
+    void chooseProduct();
+    void stop();
+    void process();
+}
