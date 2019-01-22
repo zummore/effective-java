d0c3ab98bcd38434218028a1f95b7575cb3b60a5
kingbbode <kingbbode@woowahan.com>
2019-01-22 12:26:41 +0900

    first commit

---
+++ src/main/java/com/zummore/effectivejava/EffectiveJavaApplication.java
@@ -0,0 +1,14 @@
+package com.zummore.effectivejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EffectiveJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EffectiveJavaApplication.class, args);
+    }
+
+}
+
